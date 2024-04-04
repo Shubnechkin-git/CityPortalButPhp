40cb6a8b21f94d7b2922c4f8e6d49cde39d4ec10
Даниил <danilka-shubnikov@mail.ru>
2024-04-04 22:50:28 +0400

    lol

--- ramil_city.sql
+++ ramil_city.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 04 2024 г., 21:49
+-- Время создания: Апр 04 2024 г., 21:50
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.3.26
 
@@ -63,6 +63,36 @@ INSERT INTO `task` (`id`, `user_id`, `title`, `category`, `description`, `create
 (26, 8, 'asda', 'Улицы', 'asda', '2024-04-04 22:43:07'),
 (27, 8, 'asda', 'Улицы', 'asda', '2024-04-04 22:43:35');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `patrynomic` varchar(50) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `lastname`, `firstname`, `patrynomic`, `login`, `email`, `password`, `created_at`) VALUES
+(8, 'dsfsd', 'fsdf', 'sdfsdf', 'Test', 'mail@mail.ru', '1', '2024-04-04 20:23:26'),
+(9, '23', '12312', '312312', 'Testsdfs', 'dsfsdfd@mial.ru', '1', '2024-04-04 20:24:57'),
+(10, 'dsfsdf', 'dfsd', 'sdfsdf', 'sdfsdfsd', 'sdffgygf@mail.ru', '1', '2024-04-04 20:28:20'),
+(11, 'dfsdf', 'sdfsdf', 'sdfsdf', 'Test2', 'dfgfdghf@mail.ru', '2', '2024-04-04 21:07:25'),
+(12, 'dsfsd', 'fsdfds', 'fsdfsdf', 'Test232', 'fghgfhgf@mail.ru', '3', '2024-04-04 21:56:46'),
+(13, 'vcbcvb', 'cvbcv', 'bcvbcvb', 'dfdgdg', 'sdfd6yh@mail.ru', '1', '2024-04-04 21:59:07'),
+(14, 'dfgf', 'dfg', 'dgdfg', 'Test6', 'sdfdgdf@mail.ru', '1', '2024-04-04 22:01:25');
+
 --
 -- Индексы сохранённых таблиц
 --
@@ -74,6 +104,12 @@ ALTER TABLE `task`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -82,6 +118,12 @@ ALTER TABLE `task`
 --
 ALTER TABLE `task`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
